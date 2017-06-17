b7af7ceef77b0ee9ec7a4fa446e850c9023fbae0
AleksandrStarozhilov <alternsk@gmail.com>
2017-06-17 23:09:04 +0300

    HW1a

--- src/main/java/ru/loginov/learning/Homework1a.java
+++ src/main/java/ru/loginov/learning/Homework1a.java
@@ -8,14 +8,36 @@ public class Homework1a {
         int a = 1;
         int b = 2;
         int c = 1;
+        //вычислим дискриминант D=a^2-4ac
+        double D;
+        D = Math.pow(b, 2) - (4*a * c);
+        //Если D > 0, то уравнение имеет два корня.
+        if (D > 0){
+            double x1, x2;
+            x1 = (-b - Math.sqrt(D)) / (2 * a);
+            x2 = (-b + Math.sqrt(D)) / (2 * a);
+            System.out.println("x1=" + x1);
+            System.out.println("x2=" + x2);
+        }
+        //Если D = 0, то уравнение имеет один корень.
+        else if (D == 0){
+            double x;
+            x = -b / (2 * a);
+            System.out.println("x=" + x);
+        }
+        //Если D < 0, то уравнение не имеет корней.
+        else {
+            System.out.println("Корней нет");
+        }
+
+        System.out.println("Дискриминант равен " + D);
+
+        //System.out.println(14 / (double)5); //int by int division
 
 
-        System.out.println(14 / (double)5); //int by int division
 
-
-
-        double z = Math.sqrt(9); //квадратный корень
-        System.out.println(z);
+        //double z = Math.sqrt(9); //квадратный корень
+        //System.out.println(z);
     }
 
 }
