4b4e411ea9aa0aca38589e3387b43a37118f9ed8
AleksandrStarozhilov <alternsk@gmail.com>
2017-06-18 02:05:45 +0300

    HW1b_ver1.0)

--- src/main/java/ru/loginov/learning/Homework1b.java
+++ src/main/java/ru/loginov/learning/Homework1b.java
@@ -3,7 +3,15 @@ package ru.loginov.learning;
 public class Homework1b {
 
     public static void main(String[] args) {
-        //вывести таблицу умножения 1..9 на 1..9, но только для четных множителей
+
+       for (int i=1; i<=9; i++){
+           if (i % 2 != 0) continue;
+              for(int j=1;j<=9;j++){
+               if (j % 2 != 0) continue;
+               System.out.println(i + "x" + j + "=" + i*j);
+               //вывести таблицу умножения 1..9 на 1..9, но только для четных множителей
+           }
+       }
         //2 * 2 = 4
         //2 * 4 = 8
         //...
